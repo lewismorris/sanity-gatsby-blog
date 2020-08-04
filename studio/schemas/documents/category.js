5ae1941b02f26c259977241736f0bb587619b4f3
Lewis Morris <lewis@lewismorris.co.uk>
2020-08-04 21:14:22 +0100

    Update slug field

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -11,7 +11,12 @@ export default {
     {
       name: 'slug',
       type: 'slug',
-      title: 'Slug'
+      title: 'Slug',
+      description: 'Some frontends will require a slug to be set to be able to show the post',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
     },
     {
       name: 'description',
